8d1e209e1fd2674f0a5d19627be4fac901026198
edvinvoneuler <39345445+edvinvoneuler@users.noreply.github.com>
2019-04-04 16:53:39 +0200

    script for assembly

---
+++ assembly.sh
@@ -0,0 +1,15 @@
+##!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 08:00:00
+#SBATCH -J assembly_1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user edvinvoneuler@gmail.com
+
+# Load modules
+module load bioinfo-tools megahit/1.1.2
+
+megahit -1 SRR4342129_1.paired.trimmed.fastq.gz -2 SRR4342129_2.paired.trimmed.fastq.gz -o genome_analysis_2019/data/assembly
+
+megahit -1 SRR4342133_1.paired.trimmed.fastq.gz -2 SRR4342133_2.paired.trimmed.fastq.gz -o genome_analyses_2019/data/assembly
