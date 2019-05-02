a78ed0d186134a6c7fd63e8ed275450d02e71202
edvinvoneuler <edvinvoneuler@gmail.com>
2019-05-02 14:55:14 +0200

    assembly complete

--- code/assembly.sh
+++ code/assembly.sh
@@ -3,13 +3,11 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 15:00:00
-#SBATCH -J assembly_1
+#SBATCH -J assembly_combined
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user edvinvoneuler@gmail.com
 
 # Load modules
 module load bioinfo-tools megahit/1.1.2
 
-megahit -1 ~/genome_analysis_2019/data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/genome_analysis_2019/data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz -o ~/genome_analysis_2019/data/assembly
-
-megahit -1 ~/genome_analysis_2019/data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 ~/genome_analysis_2019/data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz -o ~/genome_analysis_2019/data/assembly_33
+megahit -1 ~/genome_analysis_2019/data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,~/genome_analysis_2019/data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 ~/genome_analysis_2019/data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz, ~/genome_analysis_2019/data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz -o ~/genome_analysis_2019/data/assembly_combined
