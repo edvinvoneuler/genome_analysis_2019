0715e9e51ff4360eed03b92acc4fc2ec0af521b9
edvinvoneuler <edvinvoneuler@gmail.com>
2019-05-07 13:56:02 +0200

    parameters for assembly

--- code/binning.sh
+++ code/binning.sh
@@ -9,5 +9,4 @@
 
 load module bioinfo-tools
 load module MetaBat/2.12.1
-
-metabat -i genome_analysis_2019/data/assembly/final.contigs.fa genome_analysis_2019/data/assembly_33/final.contigs.fa -o genome_analysis_2019/data/binning
+metabat -i genome_analysis_2019/data/assembly/final.contigs.fa genome_analysis_2019/data/assembly_33/final.contigs.fa -o genome_analysis_2019/data/binning/
--- code/checkM.sh
+++ code/checkM.sh
@@ -2,15 +2,14 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 00:04:00
+#SBATCH -t 04:00:00
 #SBATCH -J edvin_checkM
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user edvinvoneuler@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load checkM
+module load CheckM
 
-# Your commands
 
- checkm lineage_wf /home/edvo1850/genome_analysis_2019/data/binning/ /home/edvo1850/genome_analysis_2019/analysis/checkM
+checkm lineage_wf -t 5 -x fa --reduced_tree /home/edvo1850/genome_analysis_2019/data/binning /home/edvo1850/genome_analysis_2019/analysis/checkM
