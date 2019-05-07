f076e3362e62df19585d3248fc7bb2ba7b0db2a3
edvinvoneuler <edvinvoneuler@gmail.com>
2019-05-07 16:21:12 +0200

    prokka loop

---
+++ code/prokka.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 15:00:00
+#SBATCH -J assembly_combined
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user edvinvoneuler@gmail.com
+
+# Load modules
+module load bioinfo-tools prokka
+
+for i in $(seq 27)
+do
+
+        if [ $i = 11 ] || [ $i = 20 ] || [ $i = 9 ]
+        then
+        prokka --kingdom Archaea /home/edvo1850/genome_analysis_2019/data/binning/binning_$1.fa --outdir /home/edvo1850/genome_analysis_2019/analyses/05_annotation
+        else
+                prokka /home/edvo1850/genome_analysis_2019/data/binning/binning_$1.fa --outdir /home/edvo1850/genome_analysis_2019/analyses/05_annotation
+        fi
+done
