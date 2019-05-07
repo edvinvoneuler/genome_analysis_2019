c74ee958746630c41e2ac53751875bb0fcf22a02
edvinvoneuler <edvinvoneuler@gmail.com>
2019-05-07 17:14:41 +0200

    outdir --force

--- code/prokka.sh
+++ code/prokka.sh
@@ -2,8 +2,8 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 15:00:00
-#SBATCH -J assembly_combined
+#SBATCH -t 3:00:00
+#SBATCH -J prokka_edvin
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user edvinvoneuler@gmail.com
 
@@ -15,8 +15,8 @@ do
 
         if [ $i = 11 ] || [ $i = 20 ] || [ $i = 9 ]
         then
-        prokka --kingdom Archaea /home/edvo1850/genome_analysis_2019/data/binning/binning_$1.fa --outdir /home/edvo1850/genome_analysis_2019/analyses/05_annotation
+        prokka --kingdom Archaea /home/edvo1850/genome_analysis_2019/data/binning/binning_$i.fa --outdir /home/edvo1850/genome_analysis_2019/analyses/05_annotation --force
         else
-                prokka /home/edvo1850/genome_analysis_2019/data/binning/binning_$1.fa --outdir /home/edvo1850/genome_analysis_2019/analyses/05_annotation
+                prokka /home/edvo1850/genome_analysis_2019/data/binning/binning_$i.fa --outdir /home/edvo1850/genome_analysis_2019/analyses/05_annotation --force
         fi
 done
