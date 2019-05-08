4ad60e798524e3714f7cdb412c0796a855668c8e
edvinvoneuler <edvinvoneuler@gmail.com>
2019-05-08 10:53:29 +0200

    8 cores, timeout at 15 with 2

--- code/assembly.sh
+++ code/assembly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -A g2019003
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 8
 #SBATCH -t 15:00:00
 #SBATCH -J assembly_combined
 #SBATCH --mail-type=ALL
@@ -12,6 +12,6 @@ module load bioinfo-tools megahit/1.1.2
 
 
 
-megahit --kmin-1pass -t 2 -1 /home/edvo1850/genome_analysis_2019/data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,/home/edvo1850/genome_analysis_2019/data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz\
+megahit --kmin-1pass -t 8 -1 /home/edvo1850/genome_analysis_2019/data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,/home/edvo1850/genome_analysis_2019/data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz\
  -2 /home/edvo1850/genome_analysis_2019/data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,/home/edvo1850/genome_analysis_2019/data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz\
  -o /home/edvo1850/genome_analysis_2019/data/assembly_combined_run
